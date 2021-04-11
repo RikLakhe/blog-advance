daa70dd15aac10dd6a70ae16ed5b04ee97f2f553
rikeshshrestha <rikeshshrestha@gmail.com>
2021-04-11 19:42:25 +0545

    updated versions to latest

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,18 @@ const config = {
         filename: 'bundle.js',
     },
     module: {
-        rules: [{ test: /\.js$/, exclude:/node_modules/, use: 'babel-loader' }],
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 };
 
